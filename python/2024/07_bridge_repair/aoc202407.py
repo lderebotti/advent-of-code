c0a8a7954eaba7c7cde9857ef498598fc26cd1ba
Lorenzo De Rebotti <lorenzo.derebotti@gmail.com>
2024-12-07 16:14:35 +0100

    day 7 cleaner

--- python/2024/07_bridge_repair/aoc202407.py
+++ python/2024/07_bridge_repair/aoc202407.py
@@ -4,125 +4,65 @@
 import pathlib
 import sys
 
+import re
+from itertools import product
+
 
 def parse_data(puzzle_input):
     """Parse input."""
-    return list(
-        [elem.strip(":") for elem in line.split()] for line in puzzle_input.splitlines()
-    )
-
-
-def evaluate_left_to_right_binary(expr):
-    tokens = []
-    num = ""
-    for c in expr:
-        if c in "+*":
-            tokens.append(int(num))
-            tokens.append(c)
-            num = ""
-        else:
-            num += c
-    tokens.append(int(num))
-    result = tokens[0]
-    i = 1
-    while i < len(tokens):
-        op = tokens[i]
-        next_num = tokens[i + 1]
-        if op == "+":
-            result += next_num
-        elif op == "*":
-            result *= next_num
-        i += 2
-    return result
+    # parse with regex
+    return [
+        (int(res), list(map(int, operators)))
+        for res, *operators in (
+            re.findall(r"\d+", line) for line in puzzle_input.splitlines()
+        )
+    ]
 
 
 def part1(data):
     """Solve part 1."""
-    accumulator = 0
+    add_mul = (lambda a, b: a + b, lambda a, b: a * b)
+    total_calib = 0
     for line in data:
-        result = int(line[0])
-
-        operators = []
-        for i in range((len(line) - 2)):
-            operators.append(["+", "*"])
-
-        for i in range(2 ** (len(line) - 2)):
-            binary = format(i, f"0{len(line) - 2}b")
-            expression = line[1]
-            for j in range(len(binary)):
-                expression += operators[j][int(binary[j])] + line[j + 2]
-            test_result = evaluate_left_to_right_binary(expression)
-            if test_result == result:
-                accumulator += test_result
+        res, operators = line
+        for operations in product(add_mul, repeat=len(operators) - 1):
+            solution = operators[0]
+            for operator, operand in zip(operations, operators[1:]):
+                solution = operator(solution, operand)
+                if solution > res:
+                    break
+                if solution == res:
+                    total_calib += res
+                    break
+            if solution == res:
                 break
-    return accumulator
 
-
-def convert_to_base(n, base, length):
-    digits = []
-    while n > 0:
-        digits.append(str(n % base))
-        n //= base
-    while len(digits) < length:
-        digits.append("0")
-    return "".join(reversed(digits))
-
-
-def evaluate_left_to_right_ternary(expr):
-    tokens = []
-    i = 0
-    while i < len(expr):
-        if expr[i : i + 2] == "||":
-            tokens.append("||")
-            i += 2
-        elif expr[i] in "+*":
-            tokens.append(expr[i])
-            i += 1
-        elif expr[i].isdigit():
-            num = ""
-            while i < len(expr) and expr[i].isdigit():
-                num += expr[i]
-                i += 1
-            tokens.append(int(num))
-        else:
-            i += 1  # Skip unknown characters
-    result = tokens[0]
-    i = 1
-    while i < len(tokens):
-        op = tokens[i]
-        next_num = tokens[i + 1]
-        if op == "+":
-            result += next_num
-        elif op == "*":
-            result *= next_num
-        elif op == "||":
-            result = int(str(result) + str(next_num))
-        i += 2
-    return result
+    return total_calib
 
 
 def part2(data):
     """Solve part 2."""
-    # TODO: implement DFS
-    accumulator = 0
+    add_mul_conc = (
+        lambda a, b: a + b,
+        lambda a, b: a * b,
+        lambda a, b: int(str(a) + str(b)),
+    )
+    total_calib = 0
     for line in data:
-        result = int(line[0])
-
-        operators = []
-        for i in range((len(line) - 2)):
-            operators.append(["+", "*", "||"])
-
-        for i in range(3 ** (len(line) - 2)):
-            ternary = convert_to_base(i, 3, len(line) - 2)
-            expression = line[1]
-            for j in range(len(ternary)):
-                expression += operators[j][int(ternary[j])] + line[j + 2]
-            evaluated_result = evaluate_left_to_right_ternary(expression)
-            if evaluated_result == result:
-                accumulator += evaluated_result
+        res, operators = line
+        for operations in product(add_mul_conc, repeat=len(operators) - 1):
+            solution = operators[0]
+            for operator, operand in zip(operations, operators[1:]):
+                solution = operator(solution, operand)
+                if solution > res:
+                    break
+                if solution == res:
+                    total_calib += res
+                    break
+            if solution == res:
                 break
 
-    return accumulator
+    return total_calib
 
 
 def solve(puzzle_input):
